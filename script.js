2072b59e761461b05b8e02906562a83fc81510e3
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-12 06:11:16 +0700

    menentukan rules

--- script.js
+++ script.js
@@ -7,9 +7,52 @@ function getPilihanKomputer() {
 };
 
 // menentukan hasil
-function getHasil(player, komp) {
+function getHasil(komp, player) {
     if (player == komp) return 'SERI!';
     if (player == 'gajah') return (komp == 'orang') ? 'MENANG!' : 'KALAH!';
     if (player == 'orang') return (komp == 'gajah') ? 'KALAH!' : 'MENANG!';
     if (player == 'semut') return (komp == 'gajah') ? 'MENANG!' : 'KALAH!';
 };
+
+// menentukan rules
+const pGajah = document.querySelector('li .gajah');
+pGajah.addEventListener('click', function () {
+    const pilihanKomputer = getPilihanKomputer();
+    const pilihanPlayer = pGajah.className;
+    const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+    const imgKomputer = document.querySelector('.img-komputer');
+    imgKomputer.setAttribute('src', 'img/' + pilihanKomputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+
+});
+
+const pOrang = document.querySelector('li .orang');
+pOrang.addEventListener('click', function () {
+    const pilihanKomputer = getPilihanKomputer();
+    const pilihanPlayer = pOrang.className;
+    const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+    const imgKomputer = document.querySelector('.img-komputer');
+    imgKomputer.setAttribute('src', 'img/' + pilihanKomputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+
+});
+
+const pSemut = document.querySelector('li .orang');
+pSemut.addEventListener('click', function () {
+    const pilihanKomputer = getPilihanKomputer();
+    const pilihanPlayer = pSemut.className;
+    const hasil = getHasil(pilihanKomputer, pilihanPlayer);
+
+    const imgKomputer = document.querySelector('.img-komputer');
+    imgKomputer.setAttribute('src', 'img/' + pilihanKomputer + '.png');
+
+    const info = document.querySelector('.info');
+    info.innerHTML = hasil;
+
+});
