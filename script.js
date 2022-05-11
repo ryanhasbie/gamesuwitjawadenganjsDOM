3431c9789d38617158b54fdb0fe3345e30847312
ryanhasbie <ryanhasbie2001@gmail.com>
2022-05-12 05:44:09 +0700

    menentukan hasil

--- script.js
+++ script.js
@@ -4,4 +4,12 @@ function getPilihanKomputer() {
     if (komp < 0.36) return 'gajah';
     if (komp >= 0.36 && komp < 0.62) return 'orang';
     return 'semut';
-}
+};
+
+// menentukan hasil
+function getHasil(player, komp) {
+    if (player == komp) return 'SERI!';
+    if (player == 'gajah') return (komp == 'orang') ? 'MENANG!' : 'KALAH!';
+    if (player == 'orang') return (komp == 'gajah') ? 'KALAH!' : 'MENANG!';
+    if (player == 'semut') return (komp == 'gajah') ? 'MENANG!' : 'KALAH!';
+};
